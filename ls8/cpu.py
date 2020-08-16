3e0cd2c5aa87a4ecc646cbaedfb232b864a70658
kly001 <karenly1961@gmail.com>
2020-08-16 13:53:19 -0400

    Updated cpu.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,8 +17,12 @@ POP     =   0b01000110
 CALL    =   0b01010000
 RET     =   0b00010001
 ADD     =   0b10100000
+CMP     =   0b10100111
+JEQ     =   0b01010101
+JMP     =   0b01010100
+JNE     =   0b01010110
 
-#  Our dev algotrithm for adding commands
+##  Algotrithm for adding commands:
 ##  add code to top
 ##  add code and function to branchtable
 ##  add function
@@ -33,39 +37,25 @@ class CPU:
         self.pc = 0             # program counter
         self.reg[7] = 0xF4      # stack pointer for push & pop operations
         self.running = True
+        self.operand_a = None
+        self.operand_b = None
+
+        self.e_flag = None
+        self.l_flag = None
+        self.g_flag = None
 
         self.branchtable = {}
-        self.branchtable[HLT] = self.hlt
-        self.branchtable[LDI] = self.ldi
-        self.branchtable[PRN] = self.prn
-        self.branchtable[PUSH] = self.push
-        self.branchtable[POP] = self.pop
-        self.branchtable[CALL] = self.call
-        self.branchtable[RET] = self.ret
+        self.branchtable[HLT]   = self.hlt
+        self.branchtable[LDI]   = self.ldi
+        self.branchtable[PRN]   = self.prn
+        self.branchtable[PUSH]  = self.push
+        self.branchtable[POP]   = self.pop
+        self.branchtable[CALL]  = self.call
+        self.branchtable[RET]   = self.ret
+        self.branchtable[JEQ]   = self.jeq
+        self.branchtable[JMP]   = self.jmp
+        self.branchtable[JNE]   = self.jne
 
-
-
-    # def load(self):
-        """Load a program into memory."""
-        # # For now, we've just hardcoded a program:
-        # address = 0
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8 (load a number into a register)
-        #     0b00000000,  # R0
-        #     0b00001000,  # 8 (value to print)
-        #     0b01000111,  # PRN R0
-        #     0b00000000,  # Register 0
-        #     0b00000001,  # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
-#------------------------------------------------------------------------------
-# # Unhardcode the Machine Code
 
     def load(self, filename):
         address = 0
@@ -90,36 +80,62 @@ class CPU:
         except FileNotFoundError:
             print(f'{sys.argv[0]}:{sys.argv[1]} not found')     
         
+
     def ram_read(self, MAR):
-        """
-        accepts address to read
-        returns value stored there
-        """
         return self.ram[MAR]
     
+
     def ram_write(self, MDR, MAR):
-        """
-        acccepts value to write and adress to write it to
-        writes value to memory at that address
-        """
         self.ram[MAR] = MDR
 
+
     def alu(self, op, operand_a, operand_b):
-        """ALU operations."""
 
         if op == ADD:
             self.reg[operand_a] += self.reg[operand_b]
+
         elif op == MUL:
                 self.reg[operand_a] *= self.reg[operand_b]
+
+        elif op == CMP:
+                registerA = self.reg[operand_a]
+                registerB = self.reg[operand_b]
+
+                if registerA == registerB:
+                    """
+                    Set Equal flag (E) to 1, otherwise set it to 0
+                    """
+                    # print("Equal")
+                    self.e_flag = 1
+                else:
+                    self.e_flag = 0
+
+                     
+                if registerA < registerB:
+                    """
+                    Set the Less-than flag (L) to 1, otherwise set it to 0
+                    """
+                    # print("Lesser")
+                    self.l_flag = 1
+                else:
+                    self.l_flag = 0
+ 
+                   
+                if registerA > registerB:
+                    """
+                    Set the Greater-than flag (G) to 1, otherwise set it to 0
+                    """
+                    # print("Greater")
+                    self.g_flag = 1
+                else:
+                    self.g_flag = 0
+
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
+
     def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
@@ -137,27 +153,22 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+
+        registers = [None] * 8
+        registers[7] = 0xF4
        
         while self.running:
-
-         #  get command
-            IR= self.ram[self.pc]   # command; where pc is
-            # we might not use these, depending on the command.  Get them just in case
+            IR= self.ram[self.pc]   
             operand_a = self.ram_read(self.pc + 1)
-            # print(operand_a)
             operand_b = self.ram_read(self.pc + 2)
-            # print(operand_b)
-
-         # update program counter
-         # look at the first two bits of the instruction
+      
 
             sets_pc_directly = IR>>4 & 0b0001
             if not sets_pc_directly:
                 self.pc += 1 + (IR>>6)
 
-        #  if IR is an ALU command, send to ALU
 
-            is_alu_command = IR & 0b00100000            #  classmate's way: check the 3rd bit
+            is_alu_command = IR & 0b00100000            #  classmate's way: checks the 3rd bit
             # is_alu_command = ((IR>>5) & 0b001) == 1   #  Tim's way: delete last 5 & check the 3rd bit
 
             if is_alu_command:
@@ -175,46 +186,60 @@ class CPU:
         print(self.reg[operand_a])
     
     def push(self, operand_a, *_):
-        #  decrement SP in R7
         self.reg[7] -= 1
-        #  Assign variable 
         sp = self.reg[7]
-        # first operand is address of register holding value
-        value = self.reg[operand_a]
-        # put that value in memory
-        self.ram[sp] = value  
-        # shortened version:
-        # #  self.ram[sp] = self.reg[operand_a] 
+        self.ram[sp] = self.reg[operand_a] 
 
     def pop (self, operand_a, *_):
-        # get value of ram[SP]
         sp = self.reg[7]
         value = self.ram[sp]
-        # write to reg[operand_a]
         self.reg[operand_a] = value
-        # increment SP
         self.reg[7] += 1
     
     def call(self, operand_a, *_):
-        # decrement the SP
         self.reg[7] -= 1
         sp = self.reg[7]
-        # get address for RET
         return_address = self.pc + 2
-        # put that value in memory
         self.ram[sp] = return_address 
 
         destination_address = self.reg[operand_a]
         self.pc = destination_address
 
     def ret(self, *_):
-        # pop from stack
         sp = self.reg[7]
         value = self.ram[sp]
-        # set PC to value popped from stack
         self.pc = value
-        # increment SP
         self.reg[7] += 1
+
+    def jmp(self, *_):
+        """
+        Jump to the address stored in the given register
+        Set PC to the address stored in the given register
+      
+        """   
+        stored_address = self.ram[self.pc + 1] 
+        self.pc = self.reg[stored_address]
+       
+    def jeq(self, *_):
+        """
+        If equal flag is set True, 
+        jump to the address stored in the given register 
+        """
+        if self.e_flag == 1:
+            self.pc = self.reg[self.ram[self.pc + 1]]
+        else:
+            self.pc += 2
+
+    def jne(self, *_):
+        """
+        If equal flag is clear(False),
+        jump to the address stored in the given register
+        """
+        if self.e_flag  == 0:
+            self.pc = self.reg[self.ram[self.pc + 1]]
+        else:
+            self.pc += 2
+
 
 
 
@@ -261,3 +286,4 @@ class CPU:
 #     print(emul.ram_read(6)) 
 #     print(emul.ram_read(7))
     
+  
--- simple.py
+++ simple.py
@@ -11,29 +11,41 @@ MULT           =  0b10100010
 ## we store everything in memory
 ## we move our PC to step through memory, and execute commands
 
-memory = [
-    PRINT_TIM,  
-    PRINT_TIM,
-    PRINT_NUM, 
-    99,
-    SAVE,        # <--- PC
-    42,  # number to save
-    2,   # register to save into
-    SAVE,
-    42,  # number to save
-    3,   # into R3
-    ADD, # registers[2] = registers[2] + registers[3]
-    2,   # Register index
-    3,   # also index
-    PRINT_REGISTER,  # should print 84
-    2,
-    HALT, 
-          ]
+import sys
+
+memory = [None] * 256
 
 running = True
 pc = 0
 
+def load_program():
+    # address = 0
+    # print(sys.argv)
+    try:
+        with open(sys.argv[1]) as file:
+            for line in file:
+                comment_split = line.split('#')
+                # print(line)
 
+                possible_num = comment_split[0]
+
+                possible_num.strip()
+
+                if possible_num == '':
+                    continue
+
+                if possible_num[0] == '1' or possible_num[0] == '0':
+                    num = possible_num[:8]
+                    # print(f'{num}: {int(num, 2)}')
+
+                    memory[address] = int(num, 2)
+                    address += 1
+
+
+    except FileNotFoundError:
+        print(f'{sys.argv[0]}:{sys.argv[1]} not found')
+
+load_program()
 
 # Memory bus
 ## a bunch of wires that the CPU uses to send an address to RAM
@@ -48,14 +60,14 @@ pc = 0
 # 0b00000010
 # 0b11111111
 
-
-
 # save the number 42 into R2
 # what arguments does SAVE require?
 
 # registers (use as variables)
 # R0-R7
+
 registers = [None] * 8
+registers[7] = 0xF4
 
 while running:
 
@@ -66,88 +78,30 @@ while running:
     if command == PRINT_TIM:
         print("Tim!")
 
-    if command == PRINT_NUM:
+    elif command == PRINT_NUM:
         number_to_print = memory[pc + 1]
         print(number_to_print)
 
-    if command == SAVE:
+    elif command == SAVE:
         num = memory[pc + 1]
         index = memory[pc + 2]
         registers[index] = num
 
-    if command == PRINT_REGISTER:
+    elif command == PRINT_REGISTER:
         reg_idx = memory[pc + 1]
         print(registers[reg_idx])
 
 
-    if command == ADD:
+    elif command == ADD:
         first_reg_idx = memory[pc + 1]
         second_reg_idx = memory[pc + 2]
 
         registers[first_reg_idx] += registers[second_reg_idx]
 
-    if command == HALT:
+    elif command == HALT:
         running = False
 
     # what if we set the pc directly?
     pc += num_operands + 1
 
 #-------------------------------------------------------------------------------
-
-# # operation Codes:
-
-# PRINT_JOAN          = 0b00000001
-# HALT                = 0b00000010
-# PRINT_NUM           = 0b00000011
-# SAVE                = 0b00000100
-# PRINT_REGISTER      = 0b00000101
-
-
-
-# memory = [
-#     PRINT_JOAN,
-#     PRINT_JOAN, 
-#     PRINT_NUM,
-#     42,
-#     SAVE,
-#     99,
-#     2,
-#     PRINT_REGISTER,
-#     2,
-#     HALT,
-# ]
-
-# # Write a program to pull each command out of memory and execute
-
-# # we can loop over it
-
-# # register aka memory
-# registers = [0] * 8
-
-# # save the number 99 into R2
-# # registers ==> R0 - R7
-
-# pc = 0 # program counter
-# running = True
-# while running:
-#     command = memory[pc]
-
-#     if command == PRINT_JOAN:
-#         print("JOAN is here !")
-
-
-#     if command == HALT:
-#         running = False
-
-#     if command == PRINT_NUM:
-#         num_to_print = memory[pc + 1]
-#         print(num_to_print)
-#         pc+= 1
-
-#     if command == SAVE:
-#         number_to_save = memory[pc +1]
-#         index = memory[pc + 2]
-#         registers[index] = number_to_save
-
-
-#     pc += 1
